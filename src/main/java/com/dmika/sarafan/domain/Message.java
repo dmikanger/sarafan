a62fec8fe3119c427369cb18f6fc854c4607cbe9
shengur_d <shengur@river-soft.net>
2019-10-25 19:27:48 +0300

    added preview for link with Open Graph Protocol

--- src/main/java/com/dmika/sarafan/domain/Message.java
+++ src/main/java/com/dmika/sarafan/domain/Message.java
@@ -2,6 +2,7 @@ package com.dmika.sarafan.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Table
 @ToString(of = {"id", "text"})
 @EqualsAndHashCode(of = {"id"})
+@Data
 public class Message {
 
     @Id
@@ -27,27 +29,15 @@ public class Message {
     @JsonView(Views.FullMessage.class)
     private LocalDateTime creationDate;
 
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String link;
 
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String linkTitle;
 
-    public Long getId() {
-        return id;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String linkDescription;
 
-    public String getText() {
-        return text;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String linkCover;
 }
---
+++ src/main/java/com/dmika/sarafan/dto/MetaDto.java
@@ -0,0 +1,16 @@
+package com.dmika.sarafan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MetaDto {
+
+    private String title;
+
+    private String description;
+
+    private String cover;
+
+}
